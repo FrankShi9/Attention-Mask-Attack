9da468b96a1af01941fe3d5e91fd51f52053803c
FrankShi9 <yunfan-shi@hotmail.com>
2023-03-08 14:12:55 +0000

    V2.4

--- GUI.py
+++ GUI.py
@@ -1,28 +1,50 @@
 import tkinter as tk
-from PIL import *
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from torchvision import transforms
+import torch.utils.data
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+import time
+from datetime import datetime
+import math
 
 
 class Demo:
     def __init__(self, root):
         self.root = root
+        self.bs = 64
+        self.nc = 10
+        self.device = torch.device("cuda:0")
+        torch.manual_seed(1)
+        self.random = True
+        self.epsilon = 0.1099
+        self.num_steps = 20
+        self.step_size = 0.005495
 
     def load_data(self):
+        test_set = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+        test_loader = DataLoader(test_set, batch_size=self.bs, shuffle=False, drop_last=True)
+
+        return test_set, test_loader
+
+    def paint_img(self):# TODO
         pass
 
-    def paint_img(self):
+    def load_model(self):# TODO
         pass
 
-    def load_model(self):
+    def infer(self):# TODO
         pass
 
-    def infer(self):
+    def attack(self):# TODO
         pass
 
-    def attack(self):
-        pass
-
-    def get_rate(self):
-        pass
+    def get_rate(self):# TODO
+        return 70
 
     def main(self):
         def callback(*args):
@@ -47,7 +69,11 @@ class Demo:
             li.place(x=290, y=40)
             li.image = img
 
-        rate = 70  # TODO
+        def paint_rate(event): # BUG
+            rate = self.get_rate()
+            tk.Label(self.root, text=str(rate) + '%', font=("arial", 10), fg="black").place(x=340, y=220)
+
+
         self.root.geometry('512x512')
         self.root.resizable(True, True)
         self.root.title('Demo')
@@ -58,9 +84,12 @@ class Demo:
         va = tk.StringVar(self.root)
         va.set("None")
         # va.trace('w', callback)
-
-        # img
         tk.OptionMenu(self.root, va, "None", "6nn", "AlexNet", command=paint_model).place(x=50, y=10)
+
+        # dropdown 2
+        v = tk.StringVar(self.root)
+        v.set("None")
+        tk.OptionMenu(self.root, v, "None", "MNIST", "cifar-10", command=paint_data).place(x=360, y=10)
 
         img2 = tk.PhotoImage(file='2.jpg').subsample(3)
         canvas.create_image(10, 250, image=img2, anchor="nw")
@@ -70,8 +99,7 @@ class Demo:
         canvas.create_image(10, 400, image=img4, anchor="nw")
         img5 = tk.PhotoImage(file='3.jpg').subsample(3)
         canvas.create_image(110, 400, image=img5, anchor="nw")
-        # img6 = tk.PhotoImage(file='mnist.jpg').subsample(6)
-        # canvas.create_image(290, 40, image=img6, anchor="nw")
+
         # Texts
         tk.Label(self.root, text="Model:", font=("arial", 10), fg="black").place(x=10, y=10)
         tk.Label(self.root, text="Dataset:", font=("arial", 10), fg="black").place(x=300, y=10)
@@ -79,20 +107,21 @@ class Demo:
         tk.Label(self.root, text="XAI mask pred:", font=("arial", 10), fg="black").place(x=100, y=200)
         tk.Label(self.root, text="pgd attack", font=("arial", 10), fg="black").place(x=10, y=350)
         tk.Label(self.root, text="disguised attack:", font=("arial", 10), fg="black").place(x=100, y=350)
-        tk.Label(self.root, text="Difference before:", font=("arial", 10), fg="black").place(x=300, y=200)
-        tk.Label(self.root, text=str(rate) + '%', font=("arial", 10), fg="black").place(x=320, y=220)
-        tk.Label(self.root, text="Difference after:", font=("arial", 10), fg="black").place(x=300, y=250)
-        tk.Label(self.root, text=str(30) + '%', font=("arial", 10), fg="black").place(x=320, y=270)
+        tk.Label(self.root, text="Difference before:", font=("arial", 10), fg="black").place(x=320, y=200)
+
+        tk.Label(self.root, text="Difference after:", font=("arial", 10), fg="black").place(x=320, y=250)
+        tk.Label(self.root, text=str(30) + '%', font=("arial", 10), fg="black").place(x=340, y=270)
 
         # attack button
-        btn_att = tk.Button(self.root, text="Attack", bd=1, command=None, bg="gray", height="1",
-                            font=("arial", 10, "bold")).place(x=230, y=256)
+        btn_att = tk.Button(self.root, text="Virgin Attack", bd=1, command=paint_rate, bg="gray", height="1",
+                            font=("arial", 10, "bold")).place(x=210, y=256)
+        btn_msk = tk.Button(self.root, text="Generate Mask", bd=1, command=None, bg="gray", height="1",
+                            font=("arial", 10, "bold")).place(x=210, y=306)
+        btn_att_msk = tk.Button(self.root, text="Masked Attack", bd=1, command=None, bg="gray", height="1",
+                            font=("arial", 10, "bold")).place(x=210, y=356)
 
-        # dropdown 2
-        v = tk.StringVar(self.root)
-        v.set("None")
-        tk.OptionMenu(self.root, v, "None", "MNIST", "cifar-10", command=paint_data).place(x=360, y=10)
-
+        d, l = self.load_data()
+        print(d,l)
         # mainloop
         self.root.mainloop()
 
